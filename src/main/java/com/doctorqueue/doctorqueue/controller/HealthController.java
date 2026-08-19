76491f94c489b5df5583cf432c7b88a1384f1973
Piyush Singh <piyushsingh@Piyushs-MacBook-Air.local>
2026-08-19 10:04:32 +0530

    Remove HealthController

--- src/main/java/com/doctorqueue/doctorqueue/controller/HealthController.java
+++
@@ -1,3 +0,0 @@
-package com.doctorqueue.doctorqueue.controller;
-
-import org.springframework.web.bind.annotation.GetMapping; import org.springframework.web.bind.annotation.RestController; @RestController public class HealthController { @GetMapping("/") public String health() { return "DoctorQueue API is running"; } }
