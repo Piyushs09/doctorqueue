10d8a8ffdee09c5df0126d08621a6ad992efb11b
Piyush Singh <piyushsingh@Piyushs-MacBook-Air.local>
2026-08-19 09:30:23 +0530

    Remove Spring Security

--- src/main/java/com/doctorqueue/doctorqueue/config/SecurityConfig.java
+++ src/main/java/com/doctorqueue/doctorqueue/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.doctorqueue.doctorqueue.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,11 +11,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**").permitAll()
                         .anyRequest().permitAll()
-                );
+                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
